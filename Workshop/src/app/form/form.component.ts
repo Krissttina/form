17eca269f3bfc6b087b12b39dfebb35445b37e88
Kristina <krismetodieva123@gmail.com>
2023-07-16 22:29:02 +0300

    access the local reference

--- Workshop/src/app/form/form.component.ts
+++ Workshop/src/app/form/form.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
-export class FormComponent {
+export class FormComponent implements AfterViewInit {
+  @ViewChild('f') form: NgForm | undefined
+
+  ngAfterViewInit(){
+    console.log(this.form);
+  }
   operatingSystems: string[] = [
     'Windows 10',
     'Linux',
